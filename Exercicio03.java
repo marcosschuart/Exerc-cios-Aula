82229ec37daca173ccabb3529ee671ee47bf2c03
marcosschuart <schuartmarcos@gmail.com>
2026-03-16 00:21:03 -0300

    Add files via upload

---
+++ Exercicio03.java
@@ -0,0 +1,17 @@
+//Nome:Marcos Schuart Nobre de Lima Palhares
+//Matrícula:1261931350
+
+import java.util.*;
+public class Exercicio03 {
+public static void main(String[] args) {
+
+Scanner leia = new Scanner(System.in);
+double dolar;
+final double COTACAO = 4.95;
+
+System.out.println("Digite o valor em dólar:20 ");
+dolar = leia.nextDouble();
+
+System.out.printf("Valor Convertido: R$%.2f", dolar*COTACAO);
+}
+}
